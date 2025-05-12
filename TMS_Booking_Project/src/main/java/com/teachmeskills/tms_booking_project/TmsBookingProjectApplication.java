d5f373757a4a6ee9a503ea0f445c9db3dd8a87bf
Marharyta Amosava <mozheliukrita@gmail.com>
2025-05-12 18:14:54 +0200

    TMS_32 1)Connected JWT for Swagger requests (for required endpoints)

--- TMS_Booking_Project/src/main/java/com/teachmeskills/tms_booking_project/TmsBookingProjectApplication.java
+++ TMS_Booking_Project/src/main/java/com/teachmeskills/tms_booking_project/TmsBookingProjectApplication.java
@@ -4,32 +4,25 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Barbershop booking service",
-                description = "Diploma app for c32",
-                contact = @Contact(
-                        name = "Rita Amosava",
-                        email = "mozheliukrita@gmail.com"
-                )
-        ),
-        security = {
-                @SecurityRequirement(name = "basicAuth")
-        }
-)
+@OpenAPIDefinition(info = @Info(
+        title = "Barbershop booking service",
+        description = "Diploma app for c32",
+        contact = @Contact(
+                name = "Rita Amosava",
+                email = "mozheliukrita@gmail.com"
+        )
+))
 @SecurityScheme(
         name = "JWT",
         type = SecuritySchemeType.HTTP,
         bearerFormat = "JWT",
         scheme = "bearer"
 )
-
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 public class TmsBookingProjectApplication {
